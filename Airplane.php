ecbdf83f95029f7aaf2698aff4de2f8e1e32a280
Artur Gonçalves <108701834+arturgsz@users.noreply.github.com>
2023-04-06 22:02:44 -0300

    commit 2 - mudanças feitas pela kathelyn

---
+++ Airplane.php
@@ -0,0 +1,99 @@
+<?php
+/* Airplane.php
+ * This is the class for the Airplane object.
+ */
+
+class Airplane
+{
+    // Attributes
+    private string $manufacturer;
+    private string $model;
+    private string $registration; //MUDAR NOME NA UML
+    private int $passenger_capacity; //MUDAR NOME NA UML
+    private float $weight_capacity; //MUDAR NOME NA UML
+
+    // Constructor
+    public function __construct(string $manufacturer, string $model, string $registration, int $passenger_capacity, float $weight_capacity)
+    {
+        $this->manufacturer = $manufacturer;
+        $this->model = $model;
+
+        if(Airplane::confereRegistro($registration))
+            $this->registration = $registration;
+        else 
+            echo "Registro de aeronave inválido";
+
+        $this->passenger_capacity = $passenger_capacity;
+        $this->weight_capacity = $weight_capacity;
+    }
+
+    //função para conferir o registro
+    public function confereRegistro(string $registration) : bool
+    {
+        $prefixo = $registration[0] . $registration[1]; 
+        if(($prefixo == 'PT' || $prefixo == 'PR' || $prefixo == 'PP' || $prefixo == 'PS') &&
+          ($registration[2] == '-') && ($registration[3] >= 65 && $registration[3] <= 90) &&
+          ($registration[4] >= 65 && $registration[4] <= 90) &&
+          ($registration[5] >= 65 && $registration[5] <= 90))
+            return true;
+        else 
+            return false;
+    }
+
+    // Getters and Setters
+    public function getManufacturer()
+    {
+        return $this->manufacturer;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function getRegistration()
+    {
+        return $this->registration;
+    }
+
+    public function getPassengerCapacity()
+    {
+        return $this->passenger_capacity;
+    }
+
+    public function getWeightCapacity()
+    {
+        return $this->weight_capacity;
+    }
+
+    public function setManufacturer(string $manufacturer)
+    {
+        $this->manufacturer = $manufacturer;
+    }
+
+    public function setModel(string $model)
+    {
+        $this->model = $model;
+    }
+
+    public function setRegistration(string $registration)
+    {
+        $this->registration = $registration;
+    }
+
+    public function setPassengerCapacity(int $passenger_capacity)
+    {
+        $this->passenger_capacity = $passenger_capacity;
+    }
+
+    public function setWeightCapacity(float $weight_capacity)
+    {
+        $this->weight_capacity = $weight_capacity;
+    }
+
+    // Destructor
+    public function __destruct()
+    {
+        echo "Airplane object destroyed.";
+    }
+}
---
+++ Airport.php
@@ -0,0 +1,69 @@
+<?php
+/* Airport.php
+ * This is the class for the Airport object.
+ */
+
+class Airport
+{
+    // Attributes
+    private string $name;
+    private string $sigla;
+    private string $city;
+    private string $state;
+
+    // Constructor
+    public function __construct(string $name, string $sigla, string $city, string $state)
+    {
+        $this->name = $name;
+        $this->sigla = $sigla;
+        $this->city = $city;
+        $this->state = $state;
+    }
+
+    // Getters and Setters
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSigla()
+    {
+        return $this->sigla;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function setSigla(string $sigla)
+    {
+        $this->sigla = $sigla;
+    }
+
+    public function setCity(string $city)
+    {
+        $this->city = $city;
+    }
+
+    public function setState(string $state)
+    {
+        $this->state = $state;
+    }
+
+    // Destructor
+    public function __destruct()
+    {
+        echo "The object {$this->name} was destroyed.";
+    }
+}
